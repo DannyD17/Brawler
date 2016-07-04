ed48d04c0a542fa20f48fc0128ea1f79ed713712
Chris <hudsonchrisp@gmail.com>
2016-07-04 17:17:48 -0600

    Added Character Controller Test Scene with Scripts
    Added Layers #8 "Character" ,   #9 "Ragdoll"  ,  #10 "Damage" to project settings so Character Controller will function properly.

---
+++ Assets/Plugins/RootMotion/PuppetMaster/_DEMOS/Assets/Scripts/AnimatorIKDemo.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RootMotion.Demos {
+
+	// Using the Unity's built in Animator IK to adjust the target pose of the Puppet.
+	[RequireComponent(typeof(Animator))]
+	public class AnimatorIKDemo : MonoBehaviour {
+
+		public Transform leftHandIKTarget;
+        public Transform rightHandIKTarget;
+
+
+		private Animator animator;
+
+		void Start() {
+			animator = GetComponent<Animator>();
+		}
+
+
+		void OnAnimatorIK(int layer) {
+			animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandIKTarget.position);
+			animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
+
+            animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandIKTarget.position);
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1f);
+
+        }
+	}
+
+}
