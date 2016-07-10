b0a90b89b93519addf3b84df2bbab35ea18a94bd
Davis Foderberg <dfoderberg@gmail.com>
2016-07-09 23:12:26 -0600

    a progress update on the UI. i have merged some scripts and added a options interface.

---
+++ Assets/Scripts/UI Scripts/Options.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Options : MonoBehaviour {
+
+    public bool isPause = false;
+    public Canvas startMenu;
+    public Canvas optionsMenu;
+
+    void Start()
+    {
+        optionsMenu.enabled = false;     // on Start have the game pause menu turned off.
+
+    }
+
+    // Update is called once per frame
+    void LoadCanvas()
+    {
+        optionsMenu.enabled = true;
+        startMenu.enabled = false;
+    }
+
+
+
+}
+	// control volume and other stuff
+
--- Assets/Scripts/UI Scripts/Pause.cs
+++ Assets/Scripts/UI Scripts/Pause.cs
@@ -5,15 +5,21 @@ using System.Collections;
 public class Pause : MonoBehaviour {
 
     public bool isPause = false;
+    public Canvas pauseMenu;
+    
 
     void Start ()
     {
-        GetComponent<Canvas>().enabled = false;     // on Start have the game pause menu turned off.
+        
+        pauseMenu.enabled = false;     // on Start have the game pause menu turned off.
 
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!! must change keycode to controler input.
+
         if (Input.GetKeyDown(KeyCode.Escape))    // when esc pressed in game the game will go to the pause menu
         {
             isPause = !isPause;                  // changes the bool that describes if the game is paused
@@ -22,16 +28,22 @@ public class Pause : MonoBehaviour {
             {
                 Debug.Log("game has been paused");
                 Time.timeScale = 0;                       // pauses the game 
-                GetComponent<Canvas>().enabled = true;  // turn on the canvas in front of the game
+                pauseMenu.enabled = true;  // turn on the canvas in front of the game
                
             }
             else
             {
-                GetComponent<Canvas>().enabled = false;   // turns of canvas
+                pauseMenu.enabled = false;   // turns of canvas
                 Time.timeScale = 1;                       // resumes game
             }
         }
     }
 
-  
+    public void Resume()    // a resume script for the resume button on pause canvas.
+    {
+        isPause = !isPause;
+        pauseMenu.enabled = false;  // turns pause screen off
+        Time.timeScale = 1;                               // resumes time
+    }
+
 }
