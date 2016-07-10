73e77703c05d2af0f3e94d9c6957f841accf20b3
Davis Foderberg <dfoderberg@gmail.com>
2016-07-09 23:59:28 -0600

    I have now added the functionality to switch between start scene and game Scene

--- Assets/Scripts/UI Scripts/Options.cs
+++ Assets/Scripts/UI Scripts/Options.cs
@@ -9,19 +9,26 @@ public class Options : MonoBehaviour {
 
     void Start()
     {
-        optionsMenu.enabled = false;     // on Start have the game pause menu turned off.
+        optionsMenu.enabled = false;     // on Start have the options menu turned off.
 
     }
 
-    // Update is called once per frame
-    void LoadCanvas()
+    public void LoadCanvas()               // brings the options menu up
     {
+        Debug.Log(" i am here");
         optionsMenu.enabled = true;
         startMenu.enabled = false;
+    }
+
+    public void backToStartCanvas ()  // bring the start menu back up
+    {
+        Debug.Log(" i am now 2 here");
+        startMenu.enabled = true;
+        optionsMenu.enabled = false;
     }
 
 
 
 }
-	// control volume and other stuff
+	// control volume and other stuff. i would like this script to be called both in the pause menu and the pregame menu. i am going to start with it just in pregame.
 
--- Assets/Scripts/UI Scripts/Startscript.cs
+++ Assets/Scripts/UI Scripts/Startscript.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class Startscript : MonoBehaviour {
+    public Scene level1;
 
     public void onStartClick()
-    {  
-        SceneManager.LoadScene(2);  // load scene by name
+    {
+        SceneManager.LoadScene(1);  // scene one is the second in build priority
+        
+      //  SceneManager.SetActiveScene(level1);  // should set the game scene to the active scene.
        
         // Debug.Log("start button was pressed");
 
