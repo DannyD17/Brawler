543256c9a0692a740888cf8deb6239759005721d
Daniel Debevec <daniel.debevec@gmail.com>
2016-07-21 19:57:46 -0600

    Added player damage class.

---
+++ Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDamage {
+
+	public float player_health = 100;
+	public float damage_multiplier = 1f;
+	public LayerMask mask; //= -1;
+
+
+	void OnCollisionEnter (Collision other)
+	{
+		if (other.gameObject.LayerMask == mask)
+			PlayerTakeDamage ((int)(other.relativeVelocity.magnitude * damage_multiplier));
+	}
+
+	public void PlayerTakeDamage(int damage){
+		player_health -= damage;
+	}
+
+
+
+
+}
